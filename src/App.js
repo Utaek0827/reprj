6b5034bf3af52f377ed04e9f18f73c2a2d50376d
utaek <utaek777@gmail.com>
2023-11-06 14:43:46 +0900

    111

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data3, setData3] = useState(null); // 초기값은 null 또는 다른 값으로 설정
+
+  useEffect(() => {
+    // useEffect를 사용하여 데이터를 가져오고 data3를 업데이트
+    axios.get('/api/ttt')
+      .then(function (response) {
+        console.log(response.data)
+        setData3(response.data);
+        
+
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }, []); // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때만 실행
+
+  console.log("되는거야1?");
+    
+  console.log("되는거야2?");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+
     </div>
   );
 }
